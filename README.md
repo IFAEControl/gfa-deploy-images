f1c407e531e468028544350e060ee01f3be0e605
David Roman <droman@ifae.es>
2020-10-27 16:54:13 +0100

    switch to pip install

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 ### Client side
 
+To install client components check that you have python installed the virtualenv package. In ubuntu and debian systems it's called python3.6-venv.
+
 To install client components of a release use the next script:
 
-`utils/deploy_client.sh <dir> <release>`
+`utils/deploy_client.sh <dir>`
 
-where **dir** is the directory where the python virtual environment will be created and release the release folder, for example: `utils/deploy_client.sh /tmp 201810160939`
+where **dir** is the directory where the python virtual environment will be created. Example usage: `utils/deploy_client.sh /tmp`
 
-This script will install all the libraries and dependencies. To activate this virtual environment we simply have to execute `source <dir>/gfa_venv/bin/activate`
+This script will install all the libraries and dependencies. To activate this virtual environment execute `source <dir>/gfa_venv/bin/activate`
 
 
 ### Server side
--- utils/deploy_client.sh
+++ utils/deploy_client.sh
@@ -3,7 +3,6 @@
 # TODO: Add die
 
 ROOT="$1"
-RELEASE="$2"
 
 echo "" > /tmp/gfa_venv_pip_log
 
@@ -16,54 +15,19 @@ function die() {
     exit 1
 }
 
-function install() {
-    (
-        cd "$1" || exit 1
-	if stat requirements.txt &> /dev/null; then
-		QT_API=pyqt5 pip install -r requirements.txt &>> /tmp/gfa_venv_pip_log || exit 1
-	fi
-
-        if stat setup.py &> /dev/null; then
-		python3 setup.py install &>> /tmp/gfa_venv_pip_log || exit 1
-	fi
-    ) || die
-}
-
 echo "Activating python3 venv"
 mkdir -p "$ROOT/gfa_venv" || die
 python3 -m venv "$ROOT/gfa_venv" || die
 source "$ROOT/gfa_venv/bin/activate"  || die
 
-echo "Copying gfa related libraries"
-cp "$RELEASE"/*.zip "$ROOT/gfa_venv" || die
-cd "$ROOT/gfa_venv" || die
-rm -rf gfagui gfa*functionality gfa*accesslib
-
-echo "Unziping libraries"
-for i in *.zip; do
-    unzip "$i" -d "$(basename $i .zip)" &> /dev/null   || die
-done
-
 echo "Installing libraries and dependencies"
-
-pip install numpy &>> /tmp/gfa_venv_pip_log || die
-
-# some pip packages are broken, so we install dependencies manually
+# guiqwt pip package is broken, so we install dependencies manually
 echo -e "\tApplying workarounds for broken pip packages"
-pip install sip &>> /tmp/gfa_venv_pip_log || die
+pip install numpy &>> /tmp/gfa_venv_pip_log || die
 pip install PyQt5 Cython &>> /tmp/gfa_venv_pip_log || die
 QT_API=pyqt5 pip install guidata &>> /tmp/gfa_venv_pip_log || die
 
-# Install dependencies of gfa_accesslib
-# TOOD: Remove it in future releases, we should use requirements.txt
-pip install pytz
-
-git clone https://github.com/IFAEControl/pyqt_tools &> /dev/null
-install pyqt_tools || die
-
-for i in gfa*accesslib/python gfa*functionality "gfagui"; do
-    echo -e "\tInstalling $i and dependencies"
-    install "$i" || die
-done
+echo "Installing gfagui"
+pip install -U --upgrade-strategy eager gfagui &>> /tmp/gfa_venv_pip_log || die
 
 echo -ne "\a"
